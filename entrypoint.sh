d33722fa2d5b5b751e5c9d41a2fbd569ee7ee6f4
Suraj-xaosi <suraj001001001@gmail.com>
2026-01-24 23:32:52 +0530

    docker containerization completed

---
+++ entrypoint.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+set -e
+
+# Wait for Postgres to be ready
+until nc -z postgres 5432; do
+  echo "Waiting for Postgres..."
+  sleep 2
+done
+
+# Wait for Kafka (Redpanda) to be ready
+until nc -z redpanda 9092; do
+  echo "Waiting for Kafka..."
+  sleep 2
+done
+
+
+
+npx prisma migrate deploy --schema=packages/db/prisma/schema.prisma
+
+
+npx prisma generate --schema=packages/db/prisma/schema.prisma
+
+# Build the app
+npm run build
+
+# Start the app (pass all arguments)
+if [ "$1" = "web" ]; then
+  npm run start --workspace=web
+elif [ "$1" = "collector" ]; then
+  npm run start --workspace=collector
+elif [ "$1" = "worker" ]; then
+  npm run start --workspace=worker
+else
+  echo "Unknown app: $1"
+  exit 1
+fi
