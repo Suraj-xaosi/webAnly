2b9e446d09aa2d515e1386a5f5f03890f274b96b
Suraj-xaosi <suraj001001001@gmail.com>
2026-07-14 01:22:25 +0530

    fixing nav

--- apps/web/components/horizontalNavbarWrapper.tsx
+++ apps/web/components/horizontalNavbarWrapper.tsx
@@ -3,13 +3,21 @@
 import { useRouter } from "next/navigation"
 import { HorizontalNavbar } from "@workspace/ui/components/main/horizontalNavbar"
 import { ThemeSwitcher } from "@/components/theme-switcher"
+import { authClient } from "@/lib/auth-client"
 
 export function HorizontalNavbarWrapper() {
-  const router = useRouter()
+    const { data: session } = authClient.useSession()
+    const user = {
+        name: session?.user.name,
+        email: session?.user.email,
+        avatar: session?.user.image,
+    }
+    const router = useRouter()
 
   return (
     <HorizontalNavbar
       themeSwitcher={<ThemeSwitcher />}
+      user={user}
       onNavigate={(href) => router.push(href)}
     />
   )
