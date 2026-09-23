b526fc974f676819fcf7c1f4e9fc516fc7bf46d9
Suraj-xaosi <suraj001001001@gmail.com>
2026-09-24 00:45:29 +0530

    fixed domain Swith

--- apps/web/components/picker/domainSwitch.tsx
+++ apps/web/components/picker/domainSwitch.tsx
@@ -5,27 +5,29 @@ import { DomainSwitcher } from "@workspace/ui/components/domain-switcher";
 import { useAppDispatch, useAppSelector } from "@/store/hooks";
 import { setDomainId, selectDomainId } from "@/store/slices/dashboardSlice";
 
-
-function useActiveDomainId() {
-  const domainId = useAppSelector(selectDomainId);
-  const { data: domains } = useDomain();
-
-  return domainId || domains?.[0]?.id || "";
-}
-
 export default function DomainSwitch() {
   const dispatch = useAppDispatch();
+  const domainId = useAppSelector(selectDomainId);
   const { data: domains, isLoading, error } = useDomain();
-  
-  const activeDomainId = useActiveDomainId();
 
   if (isLoading) return <div>Loading domains...</div>;
   if (error)     return <div>Error: {error.message}</div>;
 
+ 
+  const defaultDomainId = domains && domains.length > 0 ? domains[0]!.id : "";
+  
+  
+  const activeDomainId = domainId || defaultDomainId;
+
+ 
+  if (defaultDomainId && !domainId) {
+    dispatch(setDomainId(defaultDomainId));
+  }
+
   return (
     <DomainSwitcher
       domains={domains ?? []}
-      activeDomainId={activeDomainId}
+      activeDomainId={activeDomainId} 
       onSelect={(id: string) => dispatch(setDomainId(id))}
     />
   );
